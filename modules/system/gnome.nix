fe0ac4bf60304d22174445abe0db39a9e8eb36e3
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2022-12-23 00:13:03 +0100

    more gnome stuff

--- modules/system/gnome.nix
+++ modules/system/gnome.nix
@@ -1,6 +1,11 @@
 {
+  services.xserver.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.enable = true;
 
   services.tlp.enable = false;
+
+  programs.dconf.enable = true;
+  environment.systemPackages = with pkgs; [gnome.adwaita-icon-theme gnomeExtensions.appindicator ];
+  services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
 }
