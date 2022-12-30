97c1c282f1f2b9ba7032a899a9106b3612ba7beb
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2022-12-30 15:22:57 +0100

    home-manager(halcyon): remove neovim language support for project-local flakes

--- modules/home/halcyon/neovim.nix
+++ modules/home/halcyon/neovim.nix
@@ -1,24 +1,4 @@
 {pkgs, ...}: let
-  languages = rec {
-    lua = with pkgs; [
-      lua
-      luajit
-      sumneko-lua-language-server
-      stylua
-    ];
-    fennel = with pkgs;
-      [
-        fennel
-        fnlfmt
-      ]
-      ++ lua;
-    nix = with pkgs; [
-      rnix-lsp
-      alejandra
-      deadnix
-      statix
-    ];
-  };
 in {
   home.packages = with pkgs;
     [
