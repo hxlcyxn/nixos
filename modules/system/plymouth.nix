aab33c9c90e014940476e409eb6281af29a65f9e
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2022-12-30 14:04:35 +0100

    module(plymouth): add experimental plymouth-themes support

--- modules/system/plymouth.nix
+++ modules/system/plymouth.nix
@@ -4,21 +4,23 @@
   pkgs,
   ...
 }: let
-  theme = "breeze";
-  plymouth-theme = lib.mkIf (theme != "breeze") pkgs.stdenv.mkDerivation {
-    name = "plymouth-theme";
+  theme = "infinite_seal";
+  plymouth-themes = pkgs.stdenv.mkDerivation {
+    name = "plymouth-themes";
     src = pkgs.fetchFromGitHub {
       owner = "adi1090x";
       repo = "plymouth-themes";
       rev = "bf2f570bee8e84c5c20caac353cbe1d811a4745f";
       sha256 = "sha256-VNGvA8ujwjpC2rTVZKrXni2GjfiZk7AgAn4ZB4Baj2k=";
     };
-    configurePkase = ''
-      mkdir -p $out/share/plymouth/themes/
-    '';
     installPhase = ''
-      cp -r $src/pack_*/${theme} $out/share/plymouth/themes
-      sed "s@\/usr\/@out\/@" $out/share/plymouth/themes/${theme}
+      mkdir -p $out/share/plymouth/themes/
+      cp -r $src/pack_*/${theme} $out/share/plymouth/themes/
+
+      chmod +w $out -R
+      find $out -type f | while read file; do
+        sed -i 's;/usr/share/plymouth;/etc/plymouth;g' $file
+      done
     '';
   };
 in {
@@ -26,7 +28,7 @@ in {
     enable = true;
     inherit theme;
     themePackages = lib.mkIf (config.boot.plymouth.theme != "breeze") [
-      plymouth-theme
+      plymouth-themes
     ];
   };
 }
