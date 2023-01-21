9829deadea7f00cf82bbf5e8dcaca48079229dcc
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2023-01-21 21:17:41 +0100

    feat(halcyon): add gnome extensions

--- modules/home/halcyon/gtk.nix
+++ modules/home/halcyon/gtk.nix
@@ -1,4 +1,40 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  gnomeExtensions = {
+    workspace-indicator = {
+      name = "workspace-indicator-2";
+      uuid = "horizontal-workspace-indicator@tty2.io";
+    };
+    tray-icons = {
+      name = "tray-icons-reloaded";
+      uuid = "trayIconsReloaded@selfmade.pl";
+    };
+    vitals = {
+      name = "vitals";
+      uuid = "Vitals@CoreCoding.com";
+    };
+    sound-output-device-chooser = {
+      name = "sound-output-device-chooser";
+      uuid = "sound-output-device-chooser@kgshank.net";
+    };
+  };
+
+  pkgsGnomeExtensions = lib.mapAttrsToList (_: {
+    name,
+    uuid,
+  }:
+    pkgs.gnomeExtensions."${name}")
+  gnomeExtensions;
+  dconfGnomeExtensions = lib.mapAttrsToList (_: {
+    name,
+    uuid,
+  }:
+    uuid)
+  gnomeExtensions;
+in {
   gtk = {
     enable = true;
 
@@ -33,5 +69,15 @@
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
     };
+    "org/gnome/desktop/wm/preferences" = {
+      resize-with-right-button = true;
+      action-middle-click-titlebar = "lower";
+    };
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      enabled-extensions = dconfGnomeExtensions;
+    };
   };
+
+  home.packages = pkgsGnomeExtensions;
 }
