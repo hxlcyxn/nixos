01981e2440545b28cf14e6da4f955da6d78ee631
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2023-01-22 00:06:44 +0100

    feat: add `yubikey-touch-detector`

--- modules/system/yubikey.nix
+++ modules/system/yubikey.nix
@@ -1,7 +1,11 @@
-{
+{pkgs, ...}: {
   security.pam.services = {
     login.u2fAuth = true;
     sudo.u2fAuth = true;
     polkit1.u2fAuth = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    yubikey-touch-detector
+  ];
 }
