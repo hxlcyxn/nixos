ac22ae51579eac5f9a2a89f1cbf5ad5e226d4526
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2023-01-27 20:53:02 +0100

    feat(halcyon): add rbw

---
+++ modules/home/halcyon/rbw.nix
@@ -0,0 +1,10 @@
+{
+  programs.rbw = {
+    enable = true;
+    settings = {
+      email = "jonas.seifert04@gmail.com";
+      lock_timeout = 60 * 60; # in s
+      pinentry = "gnome3";
+    };
+  };
+}
