f633487aa54afeac653fbc920315927e55965147
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2023-08-06 11:26:59 +0200

    feat: add user to video role on laptop

--- modules/system/laptop.nix
+++ modules/system/laptop.nix
@@ -13,4 +13,11 @@
     initialPassword = "1234";
     shell = pkgs.zsh;
   };
+
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chgrp video /sys/class/backlight/%k/brightness"
+    ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chmod g+w /sys/class/backlight/%k/brightness"
+    ACTION=="add", SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chgrp video /sys/class/leds/%k/brightness"
+    ACTION=="add", SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chmod g+w /sys/class/leds/%k/brightness"
+  '';
 }
