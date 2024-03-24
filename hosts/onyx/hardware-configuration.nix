5fe1f62de10e59f218afe03e4a9ed9ecaf3fb288
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2024-03-24 12:46:11 +0100

    enable zram

--- hosts/onyx/hardware-configuration.nix
+++ hosts/onyx/hardware-configuration.nix
@@ -30,6 +30,10 @@
     {device = "/dev/disk/by-uuid/8e4ce8c3-2074-4dba-85ea-fceb937d5fa3";}
   ];
 
+  zramSwap = {
+    enable = true;
+  };
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
