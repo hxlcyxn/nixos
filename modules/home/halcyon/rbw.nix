7ec215047dea4d0174507abf88991eddc7797f71
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2024-04-14 13:51:12 +0200

    configure rbw again

--- modules/home/halcyon/rbw.nix
+++ modules/home/halcyon/rbw.nix
@@ -3,8 +3,9 @@
     enable = true;
     settings = {
       email = "jonas.seifert04@gmail.com";
+      base_url = "https://bw.monono.me/";
       lock_timeout = 60 * 60; # in s
-      pinentry = "gnome3";
+      sync_interval = 10 * 60;
     };
   };
 }
