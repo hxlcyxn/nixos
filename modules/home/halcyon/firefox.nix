8998345d861734bf2df67c6b4680e0bae800ac22
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2024-05-26 13:52:22 +0200

    add firefox gnome theme

--- modules/home/halcyon/firefox.nix
+++ modules/home/halcyon/firefox.nix
@@ -1,14 +1,32 @@
 {
+  home.file."firefox-gnome-theme" = {
+    target = ".mozilla/firefox/default/chrome/firefox-gnome-theme";
+    source = (fetchTarball {
+      url = "https://github.com/rafaelmardojai/firefox-gnome-theme/archive/refs/tags/v126.tar.gz";
+      sha256 = "1r6vvhzk8gwhs78k54ppsxzfkw7lbldjivydy87ij6grj3cf6mld";
+    });
+  };
   programs.firefox = {
     enable = true;
     profiles = {
-      "$config.home.username" = {
+      default = {
         id = 0;
         search = {
           default = "DuckDuckGo";
           order = ["DuckDuckGo" "Google"];
           force = true;
         };
+        settings = {
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "browser.tabs.drawInTitlebar" = true;
+          "svg.context-properties.content.enabled" = true;
+        };
+        userChrome = ''
+          @import "firefox-gnome-theme/userChrome.css";
+        '';
+        userContent = ''
+          @import "firefox-gnome-theme/userContent.css";
+        '';
       };
     };
   };
